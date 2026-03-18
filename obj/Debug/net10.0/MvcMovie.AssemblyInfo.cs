600513435addc183c149db56cbc50cabfd8b86e9
pearlrhema <pearlrhema8@gmail.com>
2026-03-18 14:16:13 +0100

    adding search functionality

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08eb3dadf7ab187d95ec86e999b351d360af161e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+627aeeda41d72a43c1c1cfd3d0894a2d8a826f95")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
