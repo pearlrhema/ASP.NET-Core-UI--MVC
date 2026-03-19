577a0878d7940a08f774d8357f97a73d6b559554
pearlrhema <pearlrhema8@gmail.com>
2026-03-19 10:31:31 +0100

    Recreated Database Table

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c616106f5ee3299d856b3125394ec2ec3c081e1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b1fa4205e4d05e4cfc21d79aa64bee865c7fa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
